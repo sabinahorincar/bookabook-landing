b2bf92d56a80fc4ddc36967ebb6b8867eecc3ee0
Sabina <sabina.horincar@gmail.com>
2019-11-10 18:49:15 +0200

    change url

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "https://formspree.io/mjvvyekd",
         type: "POST",
         data: {
           name: name,
